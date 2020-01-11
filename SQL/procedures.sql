d61d1d51cde0a745cf5d5eaf6bd85fb15b78b070
Aluno <Aluno@DESKTOP-BAHA1UT>
2020-01-11 17:36:12 -0200

    Procedures Incluir Pessoa

---
+++ SQL/procedures.sql
@@ -0,0 +1,26 @@
+--------------------------------------------------------
+--  Arquivo criado - Sábado-Janeiro-11-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Procedure INCLUIR_PESSOA
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE NONEDITIONABLE PROCEDURE "T20WPS2"."INCLUIR_PESSOA" (
+	vNOME  VARCHAR2,
+	vEMAIL VARCHAR2,
+	vSENHA VARCHAR2)
+	 
+IS
+vEXCEPTION EXCEPTION;
+BEGIN
+	
+	INSERT INTO T20WPS2.TB_PESSOA (COD_PESSOA, vNOME, vEMAIL, vSENHA) VALUES (T20WPS2.SQ_PESSOA_1.NEXTVAL, vNOME, vEMAIL, vSENHA)
+	
+	EXCEPTION
+		WHEN vEXCEPTION THEN
+		 RAISE_APPLICATION_ERROR(-20999,'ATENÇÃO! Falha na inclusão de pessoa', FALSE);
+		
+END
+
+/
